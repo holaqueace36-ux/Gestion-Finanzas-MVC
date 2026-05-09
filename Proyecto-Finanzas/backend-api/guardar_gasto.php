3650566535d5fb0813ed4815df8f23831f281b47
juan prieto <holaqueace36@gmail.com>
2026-05-08 21:26:42 -0500

    Update guardar_gasto.php

--- Proyecto-Finanzas/backend-api/guardar_gasto.php
+++ Proyecto-Finanzas/backend-api/guardar_gasto.php
@@ -6,8 +6,7 @@ $conn = new mysqli("localhost", "root", "", "gestion_finanzas");
 $data = json_decode(file_get_contents("php://input"), true);
 $monto = $data['monto'];
 $descripcion = $data['descripcion'];
-$usuario_id = 1; // Por ahora lo dejamos fijo para tu prueba de mañana
-
+$usuario_id = 1; 
 $sql = "INSERT INTO gastos (usuario_id, descripcion, monto) VALUES ('$usuario_id', '$descripcion', '$monto')";
 
 if ($conn->query($sql) === TRUE) {
